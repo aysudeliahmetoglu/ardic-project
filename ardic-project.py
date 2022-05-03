2528eeb6aa8e7d2cb8db54cfacf01b0f45e747f7
aysudeliahmetoglu <aysudeliahmetoglu@gmail.com>
2022-05-03 04:26:41 +0300

    operations

--- ardic-project.py
+++ ardic-project.py
@@ -1,6 +1,46 @@
-from collections import deque
 
-stack=deque()
+# from collections import deque
+
+# stack=deque
+
+# operations=input("please enter operations:")
+# stack.__add__(self, other)(operations)
+# size=stack.__sizeof__(self)()
+# print(size)
+
+# if stack.empty():
+#     print("geçersiz işlem")
+# # print(stack)
+# # for i in range(1,size):
+# #     print(i)
+
+from collections import deque
+myStack = deque()
+stack=[]
 operations=input("please enter operations:")
+operators={'*','/','+','-'}
 stack.append(operations)
-print(stack)
+
+myStack.append(operations[0])
+myStack.append(operations[1])
+myStack.append(operations[2])
+myStack.append(operations[3])
+myStack.append(operations[4])
+myStack=int(myStack)
+
+print(myStack)
+# for i in range(0,10):
+#     # print(myStack[i])
+#     if myStack[1]=='*':
+#         result=myStack[0]+myStack[2]
+#         print(result)
+
+#mutliply functions
+# def Carp(myStack) : 
+      
+     
+#     result = 1
+#     for x in myStack: 
+#          result = result * x  
+#     return result  
+# Carp(myStack)
